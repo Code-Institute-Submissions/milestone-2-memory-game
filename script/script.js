a001471c8af3ba6f073d2c86af482f0b9f5f67ad
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-03-19 15:26:57 +0000

    Background color for body not working, having issues with board reset function

--- script/script.js
+++ script/script.js
@@ -1,7 +1,42 @@
 const cards = document.querySelectorAll(".card");
 
-function flipCard() {
-    this.classList.toggle("flip");
+let hasTurnedCard = false;
+let resetBoard = false;
+let cardOne, cardTwo;
+
+function turnCard() {
+    this.classList.add("turn");
+
+    if (!hasTurnedCard) {
+        hasturnedCard = true;
+        cardOne = this;
+    }
+
+    cardTwo = this;
+    hasTurnedCard = false;
+
+    checkForBingo();
 }
 
-cards.forEach(card => card.addEventListener("click", flipCard));
+function checkForBingo() {
+    let bingo = cardOne.dataset.name === cardTwo.dataset.name;
+    bingo ? unableCards() : reverseCards();
+}
+
+function unableCards() {
+    cardOne.removeEventListener("click", turnCard);
+    cardTwo.removeEventListener("click", turnCard);
+}
+
+function reverseCards() {
+    resetBoard = true;
+
+    setTimeout(() => {
+        cardOne.classList.remove("turn");
+        cardTwo.classList.remove("turn");
+
+    resetBoard = false;
+    }, 1500);
+}
+
+cards.forEach(card => card.addEventListener("click", turnCard));
