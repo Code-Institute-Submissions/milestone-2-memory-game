f0c73961c49d49334f7b416d3183b84b01021005
Vladimirs Boreckis <v.boreckis@gmail.com>
2020-03-20 10:00:19 +0000

    Added a randomiser, reset timer needs fixing

--- script/script.js
+++ script/script.js
@@ -1,42 +1,63 @@
 const cards = document.querySelectorAll(".card");
 
 let hasTurnedCard = false;
-let resetBoard = false;
+let secureBoard = false;
 let cardOne, cardTwo;
 
+/* Function for turning Cards */
+
 function turnCard() {
+    if (secureBoard) return;
+    if (this === cardOne) return;
+
     this.classList.add("turn");
 
     if (!hasTurnedCard) {
-        hasturnedCard = true;
+        hasTurnedCard = true;
         cardOne = this;
+        return;
     }
 
     cardTwo = this;
-    hasTurnedCard = false;
+    secureBoard = true;
 
     checkForBingo();
 }
+/* Checking for a Match (Needs fixing) */
 
 function checkForBingo() {
-    let bingo = cardOne.dataset.name === cardTwo.dataset.name;
-    bingo ? unableCards() : reverseCards();
+    let isCorrect = cardOne.dataset.name === cardTwo.dataset.name;
+    isCorrect ? unableCards() : unturnCards();
 }
 
 function unableCards() {
     cardOne.removeEventListener("click", turnCard);
     cardTwo.removeEventListener("click", turnCard);
+
+    restartBoard();
 }
 
 function reverseCards() {
-    resetBoard = true;
-
     setTimeout(() => {
         cardOne.classList.remove("turn");
         cardTwo.classList.remove("turn");
 
-    resetBoard = false;
+    restartBoard();
     }, 1500);
 }
+
+function restartBoard() {
+    [hasTurnedCard, secureBoard] = [false, false];
+    [cardOne, cardTwo] = [null, null];
+}
+
+/* Randomising the card Pairs */
+
+(function randomise() {
+    cards.forEach(card => {
+        let secretLocation = Math.floor(Math.random() * 12);
+        card.style.order = secretLocation;
+    });
+})();
 
 cards.forEach(card => card.addEventListener("click", turnCard));
